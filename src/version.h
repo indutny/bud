c96ad9ec930a5c22e04431f667e8e3af394494bd
Fedor Indutny <fedor@indutny.com>
2014-06-28 19:47:54 +0400

    v0.19.0
    Signed-off-by: Fedor Indutny <fedor@indutny.com>

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define SRC_VERSION_H_
 
 #define BUD_VERSION_MAJOR 0
-#define BUD_VERSION_MINOR 18
-#define BUD_VERSION_PATCH 4
+#define BUD_VERSION_MINOR 19
+#define BUD_VERSION_PATCH 0
 
 #endif  /* SRC_VERSION_H_ */
